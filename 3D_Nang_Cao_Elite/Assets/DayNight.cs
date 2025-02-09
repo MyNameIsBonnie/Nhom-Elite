af3b17328b2502b324923ff4de2e3dfb2c7b4514
Duyen Nguyen <davichannel023@gmail.com>
2025-02-09 18:40:52 +0700

    dayNightSystem

--- 3D_Nang_Cao_Elite/Assets/DayNight.cs
+++ 3D_Nang_Cao_Elite/Assets/DayNight.cs
@@ -1,14 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DayNight : MonoBehaviour
 {
-    public Light sun;
-    public float daySpeed = 10f;
+    public Light directionalLight;
+
+    public float dayDurationInSeconds = 24.0f;
+    public int currentHour;
+    float currentTimeOfDay = 0.0f;
+
+    public List<SkyboxTimeMapping> timeMappings;
 
     void Update()
     {
-        sun.transform.Rotate(Vector3.right, daySpeed * Time.deltaTime);
+        currentTimeOfDay += Time.deltaTime / dayDurationInSeconds;
+        currentTimeOfDay %= 1;
+
+        currentHour = Mathf.FloorToInt(currentTimeOfDay * 24);
+
+        directionalLight.transform.rotation = Quaternion.Euler(new Vector3((currentTimeOfDay * 360) - 90, 170, 0));
+
+        //update the skybox
+        UpdateSkyBox();
     }
+
+    private void UpdateSkyBox()
+    {
+        Material currentSkybox = null;
+        foreach (SkyboxTimeMapping mapping in timeMappings)
+        {
+            if (currentHour == mapping.hour)
+            {
+                currentSkybox = mapping.skyboxMaterial;
+                break;
+            }
+        }
+
+        if (currentSkybox != null)
+        {
+            RenderSettings.skybox = currentSkybox;
+        }
+    }
+}
+[System.Serializable]
+public class SkyboxTimeMapping
+{
+    public string phaseName;
+    public int hour;
+    public Material skyboxMaterial;
 }
