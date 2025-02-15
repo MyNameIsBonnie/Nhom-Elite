771d1b8add29c028321ab2939f57209a2913f74e
Minh Tuấn <minhtuan5104@gmail.com>
2025-02-15 23:44:02 +0700

    NPC

--- 3D_Nang_Cao_Elite/Assets/Script/NPC.cs
+++ 3D_Nang_Cao_Elite/Assets/Script/NPC.cs
@@ -1,18 +1,141 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro; // Thêm dòng này để sử dụng TextMeshPro
+using Cinemachine; // Thêm dòng này để sử dụng Cinemachine
 
 public class NPC : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject conversationPanel; // Gán panel cuộc trò chuyện vào đây từ Inspector
+    public Button exitButton; // Gán button Exit vào đây từ Inspector
+    public Button continueButton; // Gán button Continue vào đây từ Inspector
+    public TextMeshProUGUI conversationText; // Gán TextMeshPro để hiển thị cuộc trò chuyện vào đây từ Inspector
+
+    [SerializeField]
+    private List<string> conversationLines = new List<string>(); // Danh sách các dòng trò chuyện
+    private int currentLineIndex = 0; // Chỉ số dòng trò chuyện hiện tại
+    private Coroutine typingCoroutine; // Coroutine cho hiệu ứng chữ chạy
+
+    public float typingSpeed = 0.05f; // Tốc độ chữ chạy (thời gian giữa mỗi ký tự)
+
+    private PlayerTest playerScript; // Biến lưu trữ script điều khiển di chuyển của người chơi
+    private CinemachineFreeLook freeLookCamera; // Biến lưu trữ FreeLook Camera của Cinemachine
+
     void Start()
     {
-        
+        // Ẩn panel cuộc trò chuyện khi bắt đầu
+        conversationPanel.SetActive(false);
+
+        // Gán sự kiện cho button
+        exitButton.onClick.AddListener(HideConversationPanel);
+        continueButton.onClick.AddListener(ShowNextLine);
+
+        // Lấy các script điều khiển di chuyển và FreeLook Camera của người chơi
+        playerScript = FindObjectOfType<PlayerTest>();
+        freeLookCamera = FindObjectOfType<CinemachineFreeLook>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        // Kiểm tra nếu player vào khu vực của NPC
+        if (other.CompareTag("Player"))
+        {
+            // Hiển thị panel cuộc trò chuyện và dòng trò chuyện đầu tiên
+            conversationPanel.SetActive(true);
+            currentLineIndex = 0;
+            if (typingCoroutine != null)
+            {
+                StopCoroutine(typingCoroutine);
+            }
+            typingCoroutine = StartCoroutine(TypeLine(conversationLines[currentLineIndex]));
+
+            // Vô hiệu hóa các script điều khiển di chuyển và FreeLook Camera của người chơi
+            if (playerScript != null)
+            {
+                playerScript.enabled = false;
+            }
+            if (freeLookCamera != null)
+            {
+                freeLookCamera.enabled = false;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // Kiểm tra nếu player rời khỏi khu vực của NPC
+        if (other.CompareTag("Player"))
+        {
+            // Ẩn panel cuộc trò chuyện
+            conversationPanel.SetActive(false);
+
+            // Kích hoạt lại các script điều khiển di chuyển và FreeLook Camera của người chơi
+            if (playerScript != null)
+            {
+                playerScript.enabled = true;
+            }
+            if (freeLookCamera != null)
+            {
+                freeLookCamera.enabled = true;
+            }
+        }
+    }
+
+    void HideConversationPanel()
+    {
+        // Ẩn panel cuộc trò chuyện
+        conversationPanel.SetActive(false);
+        if (typingCoroutine != null)
+        {
+            StopCoroutine(typingCoroutine);
+        }
+
+        // Kích hoạt lại các script điều khiển di chuyển và FreeLook Camera của người chơi
+        if (playerScript != null)
+        {
+            playerScript.enabled = true;
+        }
+        if (freeLookCamera != null)
+        {
+            freeLookCamera.enabled = true;
+        }
+    }
+
+    void ShowNextLine()
+    {
+        // Chuyển sang dòng trò chuyện tiếp theo
+        if (currentLineIndex < conversationLines.Count - 1)
+        {
+            currentLineIndex++;
+            if (typingCoroutine != null)
+            {
+                StopCoroutine(typingCoroutine);
+            }
+            typingCoroutine = StartCoroutine(TypeLine(conversationLines[currentLineIndex]));
+        }
+        else
+        {
+            // Nếu hết dòng trò chuyện, ẩn panel
+            HideConversationPanel();
+        }
+    }
+
+    IEnumerator TypeLine(string line)
+    {
+        conversationText.text = "";
+        foreach (char letter in line.ToCharArray())
+        {
+            conversationText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
     }
 }
+
+
+
+
+
+
+
+
---
+++ 3D_Nang_Cao_Elite/Assets/Script/PlayerTest.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class PlayerTest : MonoBehaviour
+{
+    public float speed = 6f;
+    public CinemachineFreeLook freeLookCamera; // Gán FreeLook Camera vào đây từ Inspector
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Lấy các giá trị đầu vào từ bàn phím
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Lấy hướng từ FreeLook Camera
+        Vector3 forward = freeLookCamera.transform.forward;
+        Vector3 right = freeLookCamera.transform.right;
+
+        // Đảm bảo chỉ di chuyển trên mặt phẳng (X-Z)
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        // Tạo vector di chuyển
+        Vector3 move = forward * moveVertical + right * moveHorizontal;
+
+        // Di chuyển player
+        characterController.Move(move * speed * Time.deltaTime);
+    }
+}
