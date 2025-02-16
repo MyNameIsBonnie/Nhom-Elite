4ad33143ad7006ccc3b60fa7c6100c0149d285f0
Minh Tuấn <minhtuan5104@gmail.com>
2025-02-16 10:30:47 +0700

    tagname

---
+++ 3D_Nang_Cao_Elite/Assets/Script/NPCNameDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NPCNameDisplay : MonoBehaviour
+{
+    private Transform trans;
+    private Vector3 offset = new Vector3(0, 180, 0);
+
+    private void Start()
+    {
+        trans = GameObject.Find("FreeLook Camera").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        transform.LookAt(trans);
+        transform.Rotate(offset);
+    }
+}
+
