b3b29c062a346001a1ac6839aa425899709c64cf
Duyen Nguyen <davichannel023@gmail.com>
2025-02-21 16:03:59 +0700

    done

--- 3D_Nang_Cao_Elite/Assets/Menu/MainMenuController.cs
+++ 3D_Nang_Cao_Elite/Assets/Menu/MainMenuController.cs
@@ -2,45 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.SceneManagement; // Import SceneManager
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Import SceneManager
 
 public class MainMenuController : MonoBehaviour
 {
     public GameObject playButton; // Nút Play
     public GameObject settingsButton; // Nút Setting
+    public GameObject storyButton;
     public TextMeshProUGUI startText; // Văn bản "Nhấn bất kỳ đâu để bắt đầu"
     public GameObject imageToShow; // Hình ảnh sẽ hiển thị
     public GameObject settingsPanel; // Panel cài đặt
+    public GameObject storyPanel; // Panel cot truyen
 
     void Start()
     {
         playButton.SetActive(true);
         settingsButton.SetActive(true);
-        imageToShow.SetActive(false);
+        imageToShow.SetActive(true);
         settingsPanel.SetActive(false); // Ẩn panel cài đặt khi bắt đầu
+        storyPanel.SetActive(false);
+        storyButton.SetActive(true);
 
     }
 
     void Update()
     {
-        /*if (Input.anyKeyDown)
-        {
-            ShowUIElements();
-        }*/
-        
+
     }
 
-    void ShowUIElements()
-    {
-        playButton.SetActive(true);
-        settingsButton.SetActive(true);
-        startText.gameObject.SetActive(false);
-        //imageToShow.SetActive(true);
-    }
 
     public void OnPlayButtonClicked()
     {
-        SceneManager.LoadScene("Map"); // Thay thế "YourGameSceneName" bằng tên scene của bạn
+        SceneManager.LoadScene("1"); // Thay thế "YourGameSceneName" bằng tên scene của bạn
     }
 
     public void OnSettingsButtonClicked()
@@ -48,6 +42,9 @@ public class MainMenuController : MonoBehaviour
         settingsPanel.SetActive(true); // Hiển thị panel cài đặt
         playButton.SetActive(false);
         settingsButton.SetActive(false);
+        imageToShow.SetActive(false);
+        storyButton.SetActive(false);
+
 
     }
 
@@ -56,6 +53,28 @@ public class MainMenuController : MonoBehaviour
         settingsPanel.SetActive(false); // Ẩn panel cài đặt khi đóng
         playButton.SetActive(true);
         settingsButton.SetActive(true);
+        imageToShow.SetActive(true);
+        storyButton.SetActive(true);
+
+    }
+
+    public void OnStory()//hien story
+    {
+        storyPanel.SetActive(true);
+        playButton.SetActive(false);
+        settingsButton.SetActive(false);
+        imageToShow.SetActive(false);
+        settingsPanel.SetActive(false);
+        storyButton.SetActive(false);
+    }
+    public void CloseStory()
+    {
+        storyPanel.SetActive(false);
+        playButton.SetActive(true);
+        settingsButton.SetActive(true);
+        imageToShow.SetActive(true);
+        settingsPanel.SetActive(false);
+        storyButton.SetActive(true);
     }
 }
 
