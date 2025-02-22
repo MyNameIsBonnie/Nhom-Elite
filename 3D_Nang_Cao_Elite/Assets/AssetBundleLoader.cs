982aa028e45a516cd855f9a4a0ddac214163299c
Duyen Nguyen <davichannel023@gmail.com>
2025-02-22 20:22:45 +0700

    thêm Asset Bundle spawn item

---
+++ 3D_Nang_Cao_Elite/Assets/AssetBundleLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AssetBundleLoader : MonoBehaviour
+{
+    //vào prefabs của GameObject đó, trong cửa sổ Inpcetor phía dưới cùng "Asset Bundle" -> new cái name của prefabs đó rồi gán vào bundleName.
+    string bundleName = "Item"; // Đặt tên bundle đúng với Asset Bundle Name
+    //gán tên gốc asset của prefab đó 
+    string[] assetNames = { "Staff03", "Staff01", "Potion" }; //tên asset
+
+    IEnumerator Start()
+    {
+        string path = Application.streamingAssetsPath + "/Bundles/" + bundleName;
+        AssetBundle bundle = AssetBundle.LoadFromFile(path);
+
+        if (bundle == null)
+        {
+            Debug.LogError("Failed to load AssetBundle!");
+            yield break;
+        }
+
+        foreach (string assetName in assetNames)
+        {
+            GameObject prefab = bundle.LoadAsset<GameObject>(assetName);
+            Instantiate(prefab);
+        }
+
+        bundle.Unload(false);
+    }
+}
+
